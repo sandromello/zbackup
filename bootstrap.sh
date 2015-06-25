d634ff6ff52fd651a9e963830d62be1b67ac8219
Sandro Mello <sandro.mello@inova.net>
2015-06-25 00:14:38 -0300

    bootstrap update

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,14 +11,18 @@ fi
 
 command -v curl >/dev/null 2>&1 || { echo >&2 "Requires curl. Aborting..."; exit 1; }
 
-mkdir /opt/zimbra/zbackup 2> /dev/null
-curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/conf/zbackup.ini -o /opt/zimbra/conf/zbackup.ini
-curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/zbackup/__init__.py -o /opt/zimbra/zbackup/__init__.py
-curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/zbackup/common.py -o /opt/zimbra/zbackup/common.py
+if [ ! -d "/opt/zimbra" ]; then
+  echo "Zimbra is not installed. Aborting..."; exit 1
+fi
 
-curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zbackup -o /opt/zimbra/bin/zbackup
-curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zrestore -o /opt/zimbra/bin/zrestore
-curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zbackupquery -o /opt/zimbra/bin/zbackupquery
+mkdir /opt/zimbra/zbackup 2> /dev/null
+curl --silent -L https://raw.githubusercontent.com/sandromello/zbackup/master/conf/zbackup.ini -o /opt/zimbra/conf/zbackup.ini
+curl --silent -L https://raw.githubusercontent.com/sandromello/zbackup/master/zbackup/__init__.py -o /opt/zimbra/zbackup/__init__.py
+curl --silent -L https://raw.githubusercontent.com/sandromello/zbackup/master/zbackup/common.py -o /opt/zimbra/zbackup/common.py
+
+curl --silent -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zbackup -o /opt/zimbra/bin/zbackup
+curl --silent -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zrestore -o /opt/zimbra/bin/zrestore
+curl --silent -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zbackupquery -o /opt/zimbra/bin/zbackupquery
 
 chown zimbra. /opt/zimbra/conf/zbackup.ini
 chmod 600 /opt/zimbra/conf/zbackup.ini
@@ -26,3 +30,4 @@ chmod 600 /opt/zimbra/conf/zbackup.ini
 chmod 755 /opt/zimbra/bin/zbackupquery
 chmod 755 /opt/zimbra/bin/zbackup
 chmod 755 /opt/zimbra/bin/zrestore
+echo "Installation completed!"
