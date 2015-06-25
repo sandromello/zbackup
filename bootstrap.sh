fed14cedbc6c98eda0555f5b4fb1dcf2a6845964
Sandro Mello <sandro.mello@inova.net>
2015-06-25 00:03:42 -0300

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ if ! $(python -c "import argparse" &> /dev/null); then
   pip install argparse
 fi
 
-command -v curl >/dev/null 2>&1 || { echo >&2 "Requires wget. Aborting..."; exit 1; }
+command -v curl >/dev/null 2>&1 || { echo >&2 "Requires curl. Aborting..."; exit 1; }
 
 mkdir /opt/zimbra/zbackup 2> /dev/null
 curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/conf/zbackup.ini -o /opt/zimbra/conf/zbackup.ini
@@ -20,7 +20,9 @@ curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zbackup
 curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zrestore -o /opt/zimbra/bin/zrestore
 curl -L https://raw.githubusercontent.com/sandromello/zbackup/master/bin/zbackupquery -o /opt/zimbra/bin/zbackupquery
 
-chown 600 /opt/zimbra/conf/zbackup.ini
-chown 755 /opt/zimbra/bin/zbackupquery
-chown 755 /opt/zimbra/bin/zbackup
-chown 755 /opt/zimbra/bin/zrestore
+chown zimbra. /opt/zimbra/conf/zbackup.ini
+chmod 600 /opt/zimbra/conf/zbackup.ini
+
+chmod 755 /opt/zimbra/bin/zbackupquery
+chmod 755 /opt/zimbra/bin/zbackup
+chmod 755 /opt/zimbra/bin/zrestore
